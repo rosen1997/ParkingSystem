8b21cd7074b547057b7124c9910f0da73be1f60d
rosen1997 <rosen_lechev@abv.bg>
2021-12-22 09:05:51 +0200

    add documentation files

--- ParkingServer/Models/EntityModels/PaymentModel.cs
+++ ParkingServer/Models/EntityModels/PaymentModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ParkingServer.Models.EntityModels
@@ -11,6 +12,8 @@ namespace ParkingServer.Models.EntityModels
         public DateTime PaymentDate { get; set; }
         public double PaymentAmount { get; set; }
         public int ParkingStatusId { get; set; }
+
+        [JsonIgnore]
         public ParkingStatusModel ParkingStatus { get; set; }
     }
 }
